d59768c2912f66346cb2803d6e87563bc8fb8bf3
Anton <me@inkwai.net>
2012-12-01 19:36:15 +0200

    Заготовка для плагина

--- _js/jquery-ui-thickbox.js
+++ _js/jquery-ui-thickbox.js
@@ -1,3 +1,16 @@
 // Thickbox realization will be added here
-$.widget('ui.thickbox', $.extend({}, $.ui.dialog.prototype));
-$.ui.thickbox.defaults = $.ui.dialog.prototype.defaults;
+$.widget('ui.thickbox', $.extend({}, $.ui.dialog.prototype, {
+    url: '',
+    ajaxLoad: false,
+    widgetEventPrefix: 'thickbox',
+    _init: function() {
+        this.element.data('dialog', this.element.data('thickbox'));
+        return $.ui.dialog.prototype._init.apply(this, arguments);
+    },
+    _create: function() {
+        $.ui.dialog.prototype._create.apply(this, arguments);
+        // TODO: по указанным кейсам реслизовать вставку кастомного контента через штуку, указанную ниже
+     // this.element.html();
+    }
+}));
+$.ui.thickbox.defaults = $.extend({}, $.ui.dialog.defaults);
