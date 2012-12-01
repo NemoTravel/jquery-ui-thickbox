3cb075c6a8f1711f5d4e9752d252af8bbe7d856f
Archiolidius <Archiolidius@gmail.com>
2012-12-01 21:36:18 +0400

    загрузка через iframe і ajax

--- _js/jquery-ui-thickbox.js
+++ _js/jquery-ui-thickbox.js
@@ -1,6 +1,6 @@
 // Thickbox realization will be added here
 $.widget('ui.thickbox', $.extend({}, $.ui.dialog.prototype, {
-    url: '',
+    url: 'http://www.dejurka.ru/category/css/',
     ajaxLoad: false,
     widgetEventPrefix: 'thickbox',
     _init: function() {
@@ -10,7 +10,22 @@ $.widget('ui.thickbox', $.extend({}, $.ui.dialog.prototype, {
     _create: function() {
         $.ui.dialog.prototype._create.apply(this, arguments);
         // TODO: по указанным кейсам реслизовать вставку кастомного контента через штуку, указанную ниже
-     // this.element.html();
+      if(this.url!=''){
+          if(this.ajaxLoad==true){
+              $.ajax({
+                  url: this.url,
+                  success: function(data, status){
+                      if(status=="success"){
+                          alert(data);
+                      }
+                  }
+
+              })
+          }
+          else{
+                $('<iframe src="'+this.url+'"></iframe>').appendTo(this.element);
+          }
+      }
     }
 }));
 $.ui.thickbox.defaults = $.extend({}, $.ui.dialog.defaults);
