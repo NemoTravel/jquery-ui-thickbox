9a733b25d524d801b1501ae912e4c0e323af7cdd
Archiolidius <Archiolidius@gmail.com>
2012-12-03 00:44:32 +0400

    ajax нормально подгружает странички на локальном сервере

--- _js/jquery-ui-thickbox.js
+++ _js/jquery-ui-thickbox.js
@@ -1,31 +1,32 @@
 // Thickbox realization will be added here
 $.widget('ui.thickbox', $.extend({}, $.ui.dialog.prototype, {
-    url: 'http://www.dejurka.ru/category/css/',
-    ajaxLoad: false,
-    widgetEventPrefix: 'thickbox',
-    _init: function() {
+    url:'ajax.html',
+    ajaxLoad:true,
+    widgetEventPrefix:'thickbox',
+    _init:function () {
         this.element.data('dialog', this.element.data('thickbox'));
         return $.ui.dialog.prototype._init.apply(this, arguments);
     },
-    _create: function() {
+    _create:function () {
         $.ui.dialog.prototype._create.apply(this, arguments);
         // TODO: по указанным кейсам реслизовать вставку кастомного контента через штуку, указанную ниже
-      if(this.url!=''){
-          if(this.ajaxLoad==true){
-              $.ajax({
-                  url: this.url,
-                  success: function(data, status){
-                      if(status=="success"){
-                          alert(data);
-                      }
-                  }
+        if (this.url != '') {
+            if (this.ajaxLoad == true) {
+                $.ajax({
+                    url:this.url,
+                    success:function (data) {
+                        $(".dialog-content").html(data);
 
-              })
-          }
-          else{
-                $('<iframe src="'+this.url+'"></iframe>').appendTo(this.element);
-          }
-      }
+                    },
+                    error: function(){
+                        alert('Ajax error');
+                    }
+                })
+            }
+            else {
+                $('<iframe src="' + this.url + '"></iframe>').appendTo(this.element);
+            }
+        }
     }
 }));
 $.ui.thickbox.defaults = $.extend({}, $.ui.dialog.defaults);
